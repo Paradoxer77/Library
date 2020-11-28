9ee35734f38d34735107d65a0b0f7900d39973df
Sakshyam Chapagain <chapagainsakshyam@gmail.com>
2020-11-28 14:05:42 +0545

    Added styling to card and created book constructor

---
+++ main.js
@@ -0,0 +1,12 @@
+let library = []
+
+function Book(name, author, pages, readPages) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.readPages = readPages
+}
+
+function addToLibrary() {
+
+}
