39dfd8f4ebeb27c43c6a9e6006f9ad72b48f6a00
Sakshyam Chapagain <chapagainsakshyam@gmail.com>
2020-11-28 15:32:09 +0545

    Added function to display book info using javascript

--- main.js
+++ main.js
@@ -1,12 +1,85 @@
 let library = []
 
-function Book(name, author, pages, readPages) {
+function Book(name, author, pages, isRead) {
   this.name = name
   this.author = author
   this.pages = pages
-  this.readPages = readPages
+  this.isRead = isRead
 }
 
 function addToLibrary() {
 
 }
+
+function addToDom() {
+  const main = document.querySelector("main")
+
+  const card = document.createElement("div")
+  card.classList.add("card")
+
+
+  const text = document.createElement("div")
+  const h2 = document.createElement("h2")
+
+  h2.classList.add('info-text')
+  h2.textContent = "Jojo"
+  text.appendChild(h2)
+
+  const h3 = document.createElement("h3")
+  h3.classList.add('info-text')
+  h3.textContent = "Araki"
+  text.appendChild(h3)
+
+  const pages = document.createElement("span")
+  pages.classList.add('info-text')
+  pages.textContent = "100"
+  text.appendChild(pages)
+
+  card.appendChild(text)
+
+
+  const read = document.createElement("div")
+  const buttonRead = document.createElement("button")
+
+  buttonRead.classList.add("read")
+  buttonRead.textContent = "Read"
+
+  read.appendChild(buttonRead)
+  card.appendChild(read)
+
+
+  const selectContainer = document.createElement("div")
+  const rating = document.createElement('div')
+  rating.textContent = "Rating"
+  const select = document.createElement("select")
+
+  select.classList.add("rating")
+  for (i = 1; i <= 10; i++) {
+    const option = document.createElement("option")
+
+    option.text = i
+    select.add(option)
+  }
+
+  selectContainer.appendChild(rating)
+  selectContainer.appendChild(select)
+  card.appendChild(selectContainer)
+
+
+  const deleteContainer = document.createElement("div")
+  const deleteItem = document.createElement("button")
+
+  const image = document.createElement("img")
+  image.src = "https://image.flaticon.com/icons/png/512/61/61848.png"
+  image.style.width = "24px"
+  deleteItem.appendChild(image)
+  deleteItem.classList.add("delete")
+
+
+  deleteContainer.appendChild(deleteItem)
+  card.appendChild(deleteContainer)
+
+
+  main.appendChild(card)
+}
+
